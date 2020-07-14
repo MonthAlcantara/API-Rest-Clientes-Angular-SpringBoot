42aa39b3f265553ffd1122e5e650a8ac8639066a
Montival Junior <montival_junior@yahoo.com.br>
2020-07-14 20:23:10 -0300

    Criadas classes da camada service do projeto

---
+++ src/main/java/io/github/monthalcantara/service/implementacao/ClienteServiceImpl.java
@@ -0,0 +1,8 @@
+package io.github.monthalcantara.service.implementacao;
+
+import io.github.monthalcantara.service.interfaces.ClienteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+}
---
+++ src/main/java/io/github/monthalcantara/service/implementacao/ServicoServiceImpl.java
@@ -0,0 +1,8 @@
+package io.github.monthalcantara.service.implementacao;
+
+import io.github.monthalcantara.service.interfaces.ServicoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicoServiceImpl implements ServicoService {
+}
