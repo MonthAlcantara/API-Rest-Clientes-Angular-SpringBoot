8160810b2a9d628c03882afad74405cd5c66fcf4
Montival Junior <montival_junior@yahoo.com.br>
2020-07-14 20:09:25 -0300

    Criada entity de Cliente

---
+++ src/main/java/io/github/monthalcantara/model/Cliente.java
@@ -0,0 +1,30 @@
+package io.github.monthalcantara.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "O campo nome não pode ser vazio")
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    @NotEmpty(message = "Obrigatório informar o CPF")
+    private String cpf;
+
+    private LocalDate dataCadastro;
+}
