80f64e50eaa9460c6ded79b6f4175d7e304bceaa
Montival Junior <montival_junior@yahoo.com.br>
2020-07-14 19:57:49 -0300

    Criação da classe main

---
+++ src/main/java/io/github/monthalcantara/ClientesApplication.java
@@ -0,0 +1,12 @@
+package io.github.monthalcantara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApplication.class, args);
+
+    }
+}
