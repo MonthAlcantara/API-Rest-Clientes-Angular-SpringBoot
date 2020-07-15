b06156ff00a41f0e0118a40b129737f9c5661919
Montival Junior <montival_junior@yahoo.com.br>
2020-07-14 22:05:58 -0300

    Adicionadas classes da camada de serviço

--- src/main/java/io/github/monthalcantara/service/implementacao/ClienteServiceImpl.java
+++ src/main/java/io/github/monthalcantara/service/implementacao/ClienteServiceImpl.java
@@ -1,8 +1,52 @@
 package io.github.monthalcantara.service.implementacao;
 
+import io.github.monthalcantara.model.Cliente;
+import io.github.monthalcantara.repository.ClienteRepository;
+import io.github.monthalcantara.service.exceptions.RecursoNotFound;
 import io.github.monthalcantara.service.interfaces.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
+
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> findAll(Example example) {
+        return clienteRepository.findAll(example);
+    }
+
+    @Override
+    public Cliente save(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+        if(clienteOptional.isPresent()){
+            clienteRepository.deleteById(id);
+        }
+
+    }
+
+    @Override
+    public void update(Integer id, Cliente cliente) {
+        findById(id);
+        cliente.setId(id);
+        save(cliente);
+    }
+
+    @Override
+    public Cliente findById(Integer id) {
+        return clienteRepository
+                .findById(id)
+                .orElseThrow(() -> new RecursoNotFound("Cliente não encontrado"));
+    }
 }
--- src/main/java/io/github/monthalcantara/service/interfaces/ClienteService.java
+++ src/main/java/io/github/monthalcantara/service/interfaces/ClienteService.java
@@ -1,4 +1,18 @@
 package io.github.monthalcantara.service.interfaces;
 
+import io.github.monthalcantara.model.Cliente;
+import org.springframework.data.domain.Example;
+
+import java.util.List;
+
 public interface ClienteService {
+    List<Cliente> findAll(Example example);
+
+    Cliente save(Cliente cliente);
+
+    void delete(Integer id);
+
+    void update(Integer id, Cliente cliente);
+
+    Cliente findById(Integer id);
 }
