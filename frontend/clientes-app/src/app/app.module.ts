f88caaa8ecbd62d52c1c6067ba94e1596233b99e
Montival Junior <montival_junior@yahoo.com.br>
2020-07-21 20:21:03 -0300

    criado modulo de template

--- frontend/clientes-app/src/app/app.module.ts
+++ frontend/clientes-app/src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {TemplateModule} from './template/template.module'
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    TemplateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
