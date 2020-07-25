9736a71fb30bb93fa9bd02df6067da5290e6ebd9
Montival <montival_junior@yahoo.com.br>
2020-07-25 12:26:39 -0300

    Adicionado JQUERY ao projeto

--- frontend/clientes-app/src/app/app.component.ts
+++ frontend/clientes-app/src/app/app.component.ts
@@ -1,31 +1,29 @@
 import { Component, AfterViewInit } from '@angular/core';
 
-import jQuery from 'jquery'
+import jQuery from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
   title = 'clientes-app';
 
-  ngAfterViewInit(){
-    (function($) {
-      "use strict";
-  
-        var path = window.location.href; 
-          $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-              if (this.href === path) {
-                  $(this).addClass("active");
-              }
-          });
-  
-   
-      $("#sidebarToggle").on("click", function(e) {
-          e.preventDefault();
-          $("body").toggleClass("sb-sidenav-toggled");
+  ngAfterViewInit() {
+    (function ($) {
+      'use strict';
+
+      var path = window.location.href;
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function () {
+        if (this.href === path) {
+          $(this).addClass('active');
+        }
       });
-  })(jQuery);
-  
+
+      $('#sidebarToggle').on('click', function (e) {
+        e.preventDefault();
+        $('body').toggleClass('sb-sidenav-toggled');
+      });
+    })(jQuery);
   }
 }
