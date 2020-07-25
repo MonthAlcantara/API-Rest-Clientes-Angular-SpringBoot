9ec235c637b94d196af7da303191a6fa75732838
Montival <montival_junior@yahoo.com.br>
2020-07-25 10:46:16 -0300

    Adcionado Home ao projeto

--- frontend/clientes-app/src/app/app.module.ts
+++ frontend/clientes-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {TemplateModule} from './template/template.module'
+import {TemplateModule} from './template/template.module';
+import { HomeComponent } from './home/home.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
