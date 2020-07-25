a6a4c58685084a1d75690ac8181470ea1d05c6c7
Montival <montival_junior@yahoo.com.br>
2020-07-25 10:29:18 -0300

    adcionado footer ao projeto

--- frontend/clientes-app/src/app/template/template.module.ts
+++ frontend/clientes-app/src/app/template/template.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
       ]
 })
 export class TemplateModule { }
