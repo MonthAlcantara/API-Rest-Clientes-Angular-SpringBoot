20118a4af21983349ac284297d7b90eb3d941bbe
Montival <montival_junior@yahoo.com.br>
2020-08-06 20:35:39 -0300

    Adicionado model mapper ao projeto

--- backend/src/main/java/io/github/monthalcantara/clientes/dto/ClienteDTO.java
+++ backend/src/main/java/io/github/monthalcantara/clientes/dto/ClienteDTO.java
@@ -1,13 +1,9 @@
 package io.github.monthalcantara.clientes.dto;
 
-import io.github.monthalcantara.clientes.model.Cliente;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,25 +17,4 @@ public class ClienteDTO {
 
     private String cpf;
 
-    public ClienteDTO converteParaClienteDTO(Cliente cliente) {
-        return ClienteDTO.builder()
-                .id(cliente.getId())
-                .nome(cliente.getNome())
-                .cpf(cliente.getCpf())
-                .build();
-    }
-
-    public Cliente converteDTOParaCliente(ClienteDTO clienteDTO) {
-        return Cliente.builder()
-                .id(clienteDTO.getId())
-                .cpf(clienteDTO.getCpf())
-                .nome(clienteDTO.getNome())
-                .build();
-    }
-
-    public List<ClienteDTO> converteListaClientesParaDTO(List<Cliente> clientes) {
-        return clientes.stream()
-                .map(cliente -> converteParaClienteDTO(cliente))
-                .collect(Collectors.toList());
-    }
 }
