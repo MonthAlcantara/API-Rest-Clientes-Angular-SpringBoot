030f40ca61edbd4618ce2cda619b8d2648c39817
Montival <montival_junior@yahoo.com.br>
2020-08-06 20:38:04 -0300

    Adcionado Bean modelmapper

--- backend/src/main/java/io/github/monthalcantara/ClientesApplication.java
+++ backend/src/main/java/io/github/monthalcantara/ClientesApplication.java
@@ -1,7 +1,9 @@
 package io.github.monthalcantara;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableJpaAuditing
 public class ClientesApplication {
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
 
