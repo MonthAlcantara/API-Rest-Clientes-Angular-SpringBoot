d468615fa910dab3d2b54b03f4a3bd22fff41e39
Montival <montival_junior@yahoo.com.br>
2020-09-22 17:19:24 -0300

    Atualização classe principal

--- backend/src/main/java/io/github/monthalcantara/ClientesApplication.java
+++ backend/src/main/java/io/github/monthalcantara/ClientesApplication.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 @EnableJpaAuditing
 public class ClientesApplication {
 
